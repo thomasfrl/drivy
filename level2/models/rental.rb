8fb73d4eb7e5552d7e13fc99ddbbd93b7e1408c6
thomasfrl <feral.thomas@gmail.com>
2019-11-02 14:55:01 +0100

    level2 finished

--- level2/models/rental.rb
+++ level2/models/rental.rb
@@ -1,4 +1,5 @@
-class Rental < Model 
+# rental model
+class Rental < Model
   attr_accessor :car_id, :start_date, :end_date, :distance
 
   def initialize(options)
@@ -13,10 +14,35 @@ class Rental < Model
   end
 
   def price
-    car.price_per_day * duration + car.price_per_km * distance
+    duration_price + distance__price
+  end
+
+  def distance__price
+    car.price_per_km * distance
+  end
+
+  def duration_price
+    (1..duration).sum do |day_number|
+      self.class.price_coefficient(day_number) * car.price_per_day
+    end
   end
 
   def car
     @car ||= Car.find(car_id)
+  end
+
+  def self.price_coefficient(day_number)
+    price_coefficients.find do |limit_day, _|
+      day_number >= limit_day.to_s.to_i
+    end.last
+  end
+
+  def self.price_coefficients
+    {
+      '11': 0.5,
+      '5': 0.7,
+      '2': 0.9,
+      '1': 1
+    }
   end
 end
