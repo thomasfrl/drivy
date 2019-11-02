a8c41d61dca3616e163b1ecc7a42bd0cf8c9ac0f
thomasfrl <feral.thomas@gmail.com>
2019-11-02 21:27:36 +0100

    json

--- level5/lib/models/option.rb
+++ level5/lib/models/option.rb
@@ -3,4 +3,8 @@ class Option < Model
   attr_accessor :id, :rental_id, :type
   belongs_to 'rental'
   belongs_to 'equipement', foreign_key: 'type', key: 'type'
+
+  def to_json(*)
+    type.to_json
+  end
 end
--- level5/main.rb
+++ level5/main.rb
@@ -2,10 +2,14 @@ require 'active_support/inflector'
 require 'json'
 require 'date'
 
+require './lib/model'
+require './lib/models/no_db.rb'
 Dir['./**/*.rb'].each { |f| require f }
 
+seed
+
 def main
-  FileCreator.new(Rental, 'id', 'actions').process
+  FileCreator.new(Rental, 'id', 'options', 'actions').process
 end
 
 main
